c533ef68f35e4862a3d34646cea8de82cac9ccdb
David Mora <52156328+david-mora-opensky@users.noreply.github.com>
2022-02-01 16:19:59 -0500

    update

--- docs/conf.py
+++ docs/conf.py
@@ -31,5 +31,16 @@ templates_path = ['_templates']
 
 html_style = '../css/custom.css'
 
+pygments_style = 'sphinx'
+
+def setup(app):
+    app.add_stylesheet('custom.css')
+
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
